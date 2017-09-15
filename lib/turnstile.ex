ee36841d97f8f1d6c7d5e5b05635dc6e77c70233
Ryan MacGillivray <rmacg89@gmail.com>
2017-09-15 14:14:14 +0100

    Initial commit
    Signed-off-by: Dan Burnley <dan@danops.uk>

---
+++ lib/turnstile.ex
@@ -0,0 +1,18 @@
+defmodule Turnstile do
+  @moduledoc """
+  Documentation for Turnstile.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Turnstile.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
